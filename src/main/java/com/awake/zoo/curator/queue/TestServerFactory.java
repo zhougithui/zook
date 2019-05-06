e4b91cd1805f2e7e20f7c1a4231747abb5c12798
周辉 <hui.zhou@lanmaoly.com>
2019-05-06 17:14:59 +0800

    zk curator 队列使用

---
+++ src/main/java/com/awake/zoo/curator/queue/TestServerFactory.java
@@ -0,0 +1,25 @@
+package com.awake.zoo.curator.queue;
+
+import org.apache.curator.test.InstanceSpec;
+import org.apache.curator.test.TestingServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class TestServerFactory {
+    private static final Logger logger = LoggerFactory.getLogger(TestServerFactory.class);
+
+    public static TestingServer instance(){
+        InstanceSpec instanceSpec = new InstanceSpec(new File("/work/temp"),
+                2181, 2888, 3888, false, 1, 1000,
+                1000, null, "172.19.60.174");
+        try {
+            TestingServer server = new TestingServer(instanceSpec, false);
+            return server;
+        } catch (Exception e) {
+            logger.error("初始化zk失败", e);
+        }
+        return null;
+    }
+}
